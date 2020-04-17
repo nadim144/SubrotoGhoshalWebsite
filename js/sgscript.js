90e46b4eb2994ffd479a75fb94648c54f76d0267
Nadim Ahmad <nadim144@users.noreply.github.com>
2020-04-18 01:47:15 +0530

    Implement image canvace of cartoon gallery

---
+++ js/sgscript.js
@@ -0,0 +1,14 @@
+
+// Global Variable Section
+
+
+
+//All funtion definations.
+
+// function imgcanvace()
+// {
+//     var img = document.getElementById("slot-1-1");
+//     console.log("my image"+img);
+//     img.clientHeight=400;
+//     img.clientWidth400;
+// }
